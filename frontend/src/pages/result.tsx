b9461424e3125e39a849b80e21f3ed4f1f68fd22
권연욱 <w00k0805@naver.com>
2021-03-02 21:39:42 +0900

    Feat: 어쩌다 만든 부하발생기

--- frontend/src/pages/result.tsx
+++ frontend/src/pages/result.tsx
@@ -9,6 +9,16 @@ import Header from '../components/Header'
 import { useRouter } from 'next/router'
 import axios from 'axios'
 
+type mbtiResult = {
+    type: string,
+    desc: string,
+    name: string,
+    img: '',
+    lovers: [{}],
+    haters: [{}],
+    products: [{}]
+}
+
 function result() {
     const router = useRouter();
     const MBTI = router.query;
@@ -17,77 +27,63 @@ function result() {
     for(const [key, value] of Object.entries(MBTI)) {
         score[i++] = Number(value);
     }
-    const [mbti, setMBTI] = useState<object>({
+    const [mbtiResult, setMBTI] = useState<mbtiResult>({
         type: '',
         desc: '',
         name: '',
-        lovers: [],
-        haters: [],
-        products: []
+        img: '',
+        lovers: [{}],
+        haters: [{}],
+        products: [{}]
     });
 
-    // let mbti = {
-    //     type: '',
-    //     desc: '',
-    //     name: '',
-    //     lovers: [],
-    //     haters: [],
-    //     products: []
-    // }
-
-    useEffect(() => {
-        axios.get('http://localhost:8080/mbti/result', {
-            params: {
-                IE: score[0],
-                SN: score[1],
-                TF: score[2],
-                JP: score[3],
-            }
+    axios.get('http://localhost:8080/mbti/result', {
+        params: {
+            IE: score[0],
+            SN: score[1],
+            TF: score[2],
+            JP: score[3],
+        }
+    })
+        .then((res) => {
+            console.log(res.data);
+            setMBTI(res.data);
         })
-            .then((res) => {
-                console.log(res.data);
-                // setMBTI(res.data);
-            })
-            .catch((err) => { console.log(err) })
-    }, []);
-    // axios.get('http://localhost:8080/mbti/result', {
-    //     params: {
-    //         IE: score[0],
-    //         NS: score[1],
-    //         FT: score[2],
-    //         JP: score[3],
-    //     }
-    // })
-    //     .then((res) => {
-    //         // console.log(res.data);
-    //         setMBTI(res.data);
-    //         console.log(mbti);
+        .catch((err) => { console.log(err) });
+
+    // useEffect(() => {
+    //     axios.get('http://localhost:8080/mbti/result', {
+    //         params: {
+    //             IE: score[0],
+    //             SN: score[1],
+    //             TF: score[2],
+    //             JP: score[3],
+    //         }
     //     })
-    // .catch((err) => { console.log(err) })
+    //         .then((res) => {
+    //             console.log(res.data);
+    //             setMBTI(res.data);
+    //         })
+    //         .catch((err) => { console.log(err) })
+    // }, []);
     
-    const description = mbti.desc.split("|");
+    const description = mbtiResult.desc.split("|");
     const descriptions = description.map((str, idx) => <Desc desc={str} key={idx}/>)
-    const src0 = '/microwave.png'
-    const lover = mbti.lovers[0].name
-    console.log('lover: ' + mbti.lovers[0]);
-    const src1 = '/tab.jpg'
-    const hater = mbti.haters[0].name
-    const src2 = '/robo.jpg'
-    const products = ["/microwave.jpg", "/tab.jpg", "/robo.jpg"]
+    console.log('lover: ' + mbtiResult.lovers[0].name);
 
     return (
         <div>
             <Header />
             <Layout pageTitle="Result">
                 <div className={styles.wrapper}>
-                    <Title name={mbti.name} />
-                    <img src={src0} alt="" width="100" height="100" />
+                    <Title name={mbtiResult.name} />
+                    <img src={mbtiResult.img} alt="" width="100" height="100" />
                     <ul>
                         {descriptions}
                     </ul>
-                    <Pair type="환상" name={lover} src={src1} />
-                    <Pair type="환장" name={hater} src={src2} />
-                    <Recommend name={mbti.name} products={mbti.products}/>
+                    <Pair type="환상" name={mbtiResult.lovers[0].name} src={mbtiResult.lovers[0].img} />
+                    <Pair type="환장" name={mbtiResult.haters[0].name} src={mbtiResult.haters[0].img} />
+                    <Recommend name={mbtiResult.name} products={mbtiResult.products}/>
                 </div>
             </Layout>
         </div>
