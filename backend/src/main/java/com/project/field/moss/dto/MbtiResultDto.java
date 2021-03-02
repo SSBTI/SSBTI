dd5b8c247f877fe2554fac2765141e8800a0f909
DESKTOP-KVCQHCD\multicampus <010-2648-8131@daum.net>
2021-03-02 11:59:02 +0900

    [Feat] get MbtiType with Product

--- backend/src/main/java/com/project/field/moss/dto/MbtiResultDto.java
+++ backend/src/main/java/com/project/field/moss/dto/MbtiResultDto.java
@@ -1,25 +1,23 @@
 package com.project.field.moss.dto;
 
 import com.project.field.moss.domain.Mbti;
+import com.project.field.moss.domain.Product;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
 
-
+@Builder
 @Getter @Setter
-public class MbtiDto {
+@AllArgsConstructor
+public class MbtiResultDto {
     private String type;
     private String desc;
     private List<String> lovers;
     private List<String> haters;
-
-
-    public MbtiDto(Mbti mbti){
-        this.type = mbti.getType().toString();
-        this.desc = mbti.getDesc();
-
-    }
+    private List<Product> products;
 
 }
