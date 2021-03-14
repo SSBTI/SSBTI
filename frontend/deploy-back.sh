4b1b48b6aeedb84e773565ac9285a84e969832b2
hyunwoo <hw2621@daum.net>
2021-03-15 01:35:18 +0900

    Ci: Update ssbti.yml

--- frontend/deploy-back.sh
+++
@@ -1,8 +0,0 @@
-cd /home/ubuntu/field-team3/frontend
-sudo echo $dockerhubID $dockerhubPW
-sudo docker build -t jhw2621/ssbti:latest .
-sudo docker login -u $dockerhubID -p $dockerhubPW
-sudo docker push jhw2621/ssbti:latest
-
-kubectl apply -f ssbti.yml
-                          
