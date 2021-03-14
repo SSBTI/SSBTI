4381a812e15fc360caa137f8f58b107f1e5e3062
hyunwoo <hw2621@daum.net>
2021-03-14 22:37:08 +0900

    Ci: update deploy-back.sh

--- frontend/deploy-back.sh
+++ frontend/deploy-back.sh
@@ -1,5 +1,6 @@
 cd /home/ubuntu/field-team3/frontend
-
+sudo usermod -a -G docker jenkins
+sudo echo $dockerhubID $dockerhubPW
 sudo docker build -t jhw2621/ssbti:latest .
 sudo docker login -u $dockerhubID -p $dockerhubPW
 sudo docker push jhw2621/ssbti:latest
