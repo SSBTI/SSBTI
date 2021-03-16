bcaec1be6fc285e19eb6eafc27dfb02b70ffe9d6
Jubin Jang <sunshine0429@snu.ac.kr>
2021-03-16 14:02:19 +0900

    feat: edit lambda_function.py and add js files for chat service
    MVP 서비스에서 성격 유형별 count 를 증가시킬 수 있도록
    lambda_function.py를 수정했다.
    채팅 기능을 위해서 필요한 lambda function들을 추가했다.

--- AWS Lambda/lambda_function.py
+++ AWS Lambda/lambda_function.py
@@ -1,36 +1,62 @@
-import json, sys, pymysql
+import json, pymysql
 
 
 def lambda_handler(event, context):
-    result = ''
+    mbti = ''
     query = event['queryStringParameters']
     
-    if query['IE']: result += 'I'
-    else: result += 'E'
-    if query['SN']: result += 'S'
-    else: result += 'N'
-    if query['TF']: result += 'T'
-    else: result += 'F'
-    if query['JP']: result += 'J'
-    else: result += 'P'
+    if int(query['IE']) > 0: mbti += 'I'
+    else: mbti += 'E'
+    if int(query['SN']) > 0: mbti += 'S'
+    else: mbti += 'N'
+    if int(query['TF']) > 0: mbti += 'T'
+    else: mbti += 'F'
+    if int(query['JP']) > 0: mbti += 'J'
+    else: mbti += 'P'
     
     conn = pymysql.connect(host="database-1.cg9dro7y0en2.us-east-2.rds.amazonaws.com", port=3306, user="admin", password="duaghkqpswkfzhsba", db="test", charset="utf8")
     curs = conn.cursor()
-    sql = f"select * from mbti where type='{result}'"
+    # sql = f"select * from mbti where type='{mbti}'"
+    sql = f"select mbti.*, relationship.* from mbti join relationship on mbti.mbti_id = relationship.from_id where mbti.type='{mbti}' order by relationship.type;"
     curs.execute(sql)
-    result2 = curs.fetchall()
+    result = curs.fetchall()
+    
+    sql1 = f"update mbti set count = count + 1 where type='{mbti}'"
+    curs.execute(sql1)
+    
+    sql2 = f"select * from mbti where mbti.mbti_id={result[0][9]}"
+    curs.execute(sql2)
+    hater = curs.fetchall()
+    
+    sql3 = f"select * from mbti where mbti.mbti_id={result[1][9]}"
+    curs.execute(sql3)
+    lover = curs.fetchall()
+    
+    sql4 = 'select * from product order by rand() limit 5'
+    curs.execute(sql4)
+    products = curs.fetchall()
+    
+    conn.commit()
     conn.close()
     
+    product_list = []
+    for product in products:
+        temp = dict()
+        temp['id'] = product[0]
+        temp['goodsDetailUrl'] = product[4]
+        temp['imgPath1'] = product[9]
+        temp['uspDesc'] = product[15]
+        product_list.append(temp)
+    
+    
     response = dict()
-    response['desc'] = result2[0][2]
-    response['haters'] = [{"type":"INFP", "name":"남들","img":"www"}]
-    response['img'] = result2[0][3]
-    response['lovers'] = [{"type":"INFP", "name":"남들","img":"www"}]
-    response['name'] = result2[0][4]
-    response['products'] = [{"id":111,"goodsDetailUrl":"ddd","imgPath1":"www","uspDesc":"www"}]
-    response['type'] = result2[0][5]
-    
-    
+    response['desc'] = result[0][2]
+    response['haters'] = [{"type":hater[0][5], "name":hater[0][4],"img":hater[0][3]}]
+    response['img'] = result[0][3]
+    response['lovers'] = [{"type":lover[0][5], "name":lover[0][4],"img":lover[0][3]}]
+    response['name'] = result[0][4]
+    response['products'] = product_list
+    response['type'] = result[0][5]
     
     return {
         'statusCode': 200,
