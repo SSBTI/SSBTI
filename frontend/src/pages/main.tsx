cdfdf8026d4e2ab5493f0d73e8f93d5ab2d5da64
권연욱 <w00k0805@naver.com>
2021-03-19 16:34:47 +0900

    Chore: add total number in main.tsx

--- frontend/src/pages/main.tsx
+++ frontend/src/pages/main.tsx
@@ -1,21 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../styles/main.module.css';
 import Router from 'next/router';
 import List from '../components/ListMBTI';
+import axios from 'axios';
 
 function Main() {
-  // 리뷰 목록 조회 시 첫 페이지 번호
-  const page = 1;
+  type mbtiAll = {
+      type: '',
+      percent: '',
+      img: ''
+  }
+
+  const [mbti, setMBTI] = useState<Array<mbtiAll>>([{
+      type: '',
+      percent: '',
+      img: ''
+  }]);
+
+  const [constructorHasRun, setConstructorHasRun] = useState(false);
+  const [total, setTotal] = useState(0);
+  
+  // 모든 mbti 유형 불러와 setMBTI
+  const constructor = () => {
+    if (constructorHasRun) return;
+    axios.get(`${process.env.NEXT_PUBLIC_MBTI_API}/land`)
+    .then((res) => {
+      const list = res.data;
+      setMBTI(list.slice(0, 15));
+      setTotal(list.slice(16));
+      setConstructorHasRun(true);
+    })
+    .catch((err) => { console.log(err) });
+  };
+  constructor();
 
   //api 만들어지면 몇명이 이용했는지 받아온 다음 버튼에 출력
   return (
     <div className={styles.wrapper}>
       <div className={styles.title}>SSBTI 테스트</div>
       <div className={styles.description}>내가 만약에..... 삼성 가전이라면?</div>
-      <List />
+      <List mbti={mbti}/>
       <button className={styles.startButton} onClick={()=> Router.push('/survey')}>
         테스트로 알아보기
-        <div className={styles.total}>지금까지 20억 명이 참여했어요.</div>
+        <div className={styles.total}>지금까지 {total} 명이 참여했어요.</div>
       </button>
       <div>
         <button className={styles.startButton} onClick={()=>Router.push({
