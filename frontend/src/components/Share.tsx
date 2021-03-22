b5e05b6ccee59befbcc912b4c4187cc62039995d
hyunwoo <hw2621@daum.net>
2021-03-22 09:38:15 +0900

    fix: hide KAKAO key

--- frontend/src/components/Share.tsx
+++ frontend/src/components/Share.tsx
@@ -37,7 +37,8 @@ function Share() {
       // 중복 initialization 방지
       if (!kakao.isInitialized()) {
         // 두번째 step 에서 가져온 javascript key 를 이용하여 initialize
-        kakao.init('9422bac99ff0b2e4105609aabef19722')
+        
+        kakao.init(`${process.env.NEXT_PUBLIC_KAKAO_KEY}`)
       }
       kakao.Link.createDefaultButton({
         // Render 부분 id=kakao-link-btn 을 찾아 그부분에 렌더링을 합니다
