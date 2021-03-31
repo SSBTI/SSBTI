2c50f212b9c80c1d4627a837f5aad9df98194411
hyunwoo <hw2621@daum.net>
2021-04-01 02:57:14 +0900

    fix: fix loader img,title layout

--- frontend/src/components/loader.tsx
+++ frontend/src/components/loader.tsx
@@ -3,10 +3,10 @@ import styles from '../styles/loader.module.css';
 function Loader() {
   return (
   <div className={styles.loader}>
-    <img className={styles.loader__img} src="Loader/random_veryfast.gif"/>
     <div className={styles.loader__title}>
       정보를 받아오고 있습니다..
     </div>
+    <img className={styles.loader__img} src="Loader/random_veryfast.gif"/>
   </div>
   
   );
