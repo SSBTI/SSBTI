9a9253ea04b4d396f424cb6a31e80dd2c20a793e
권연욱 <w00k0805@naver.com>
2021-04-07 04:22:24 +0900

    refactor: remove useless code from reviewBoard.tsx

--- frontend/src/pages/reviewBoard.tsx
+++ frontend/src/pages/reviewBoard.tsx
@@ -1,10 +1,9 @@
-import React, { useState, useLayoutEffect } from 'react';
+import React from 'react';
 import dynamic from 'next/dynamic';
 import Layout from '../components/Layout';
 import Loader from '../components/loader';
 import Information from '../components/review/Board/information';
 import CheckIsMobile from '../hooks/checkIsMobile';
-import Router from 'next/router';
 
 const Editor = dynamic(
   () => import('../components/review/Board/editor'),
